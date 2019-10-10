9a716f83d7f168f603400b1eaa2041c2ec6ac3e4
Sergey Sachkov <43661110+sir-Gee@users.noreply.github.com>
2019-10-09 21:23:56 -0400

    updated all templates
    changed logic on routes, added delete button and sort buttons.
    changed session logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+import hashlib
 
 app = Flask(__name__)
 
@@ -32,13 +33,11 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, username, password):
-        self.email = email
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
@@ -50,23 +49,63 @@ def add_post():
         post_title = request.form['title']
         post_text = request.form['text']
 
-        if post_title == Blog.query.filter_by(title=post_title).first():
-            return redirect("/addpost?error=You have this post already")
+        if post_title == "" or post_text == "":
+            return render_template('addpost.html', errorPost="Title or Body cannot be blank")
 
-        user = User.query.filter_by(email=session['email']).first()
+
+        # if post_title == Blog.query.filter_by(title=post_title).first():
+        #     return redirect("/addpost?error=You have this post already")
+
+        user = User.query.filter_by(username=session['username']).first()
         blog = Blog(post_title, post_text, datetime.datetime.now().strftime("%c"), user)
         db.session.add(blog)
         db.session.commit()
 
-        return redirect("/")
+        blog = Blog.query.filter_by(title=post_title, text=post_text).first()
+
+        return redirect("/index?user_id="+str(user.id)+"&post_id="+str(blog.id))
 
     return render_template('addpost.html', errorTask=request.args.get("error"))
 
 
+@app.route("/blogs", methods=['POST', 'GET'])
+def blogs():
+    blog = Blog.query.all()
+    user = User.query.all()
+    return render_template('allUserPosts.html', posts=blog, user=user)
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return redirect('index')
+
+
+@app.route('/index', methods=['POST', 'GET'])
+def main_page():
+
     post_id = request.args.get('post_id')
     user_id = request.args.get('user_id')
+    sort_desc = request.args.get('sort_desc')
+    sort_asc = request.args.get('sort_asc')
+    delete_post = request.args.get('delete_post')
+
+    if delete_post:
+        blog = Blog.query.filter_by(id=post_id).first()
+        db.session.delete(blog)
+        db.session.commit()
+        user = User.query.filter_by(id=user_id).first()
+        blog = Blog.query.filter_by(owner_id=user_id).order_by(desc(Blog.created_at)).all()
+        return render_template('posts.html', posts=blog, user=user)
+
+    if sort_desc:
+        user = User.query.filter_by(id=user_id).first()
+        blog = Blog.query.filter_by(owner_id=user_id).order_by(desc(Blog.created_at)).all()
+        return render_template('posts.html', posts=blog, user_id=user_id, user=user)
+
+    if sort_asc:
+        user = User.query.filter_by(id=user_id).first()
+        blog = Blog.query.filter_by(owner_id=user_id).all()
+        return render_template('posts.html', posts=blog, user=user)
 
     if post_id:
         blog = Blog.query.filter_by(id=post_id).first()
@@ -74,10 +113,12 @@ def index():
         return render_template('post.html', post=blog, user=user)
 
     if user_id:
-        blog = Blog.query.filter_by(owner_id=user_id).all()
+        blog = Blog.query.filter_by(owner_id=user_id).order_by(desc(Blog.created_at)).all()
+        user = User.query.filter_by(id=user_id).first()
         # blog1 = blog.order_by(desc(Blog.id)).all()
         # blog = Blog.query.filter_by(owner_id=user_id).all()
-        return render_template('posts.html', posts=blog)
+        return render_template('posts.html', posts=blog, user=user)
+
 
     # all_posts = Blog.query.order_by(Blog.created_at.desc()).all()
 
@@ -91,25 +132,25 @@ def login():
     error_mail = ""
 
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
 
-        if email == "" or password == "":
-            flash('zoiks! Email and Password cant be blank')
+        if username == "" or password == "":
+            flash('zoiks! Username and Password cant be blank')
             return redirect('/login')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
 
         if user is None:
-            error_mail = 'zoiks! User with ' + email + ' doesnt exist. Please register one'
+            error_mail = 'zoiks! User with ' + username + ' doesnt exist. Please register one'
             return render_template("register.html", error_mail=error_mail)
 
-        if password != user.password:
+        if hashlib.md5(password.encode()).hexdigest() != user.password:
             flash('zoiks! Password is not correct')
             # return redirect('/login')
-            return render_template("login.html")
+            return render_template("login.html", username=username)
 
-        session['email'] = email
+        session['username'] = username
         return redirect("/addpost")
     else:
         return render_template("login.html")
@@ -120,65 +161,67 @@ def register():
     error_register = ''
 
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        if email == '' or password == '' or verify == '':
+        if username == '' or password == '' or verify == '':
             error_register = 'one or more fields are invalid'
             return render_template('register.html', error_register=error_register)
-        else:
-            if not is_email(email):
-                flash('zoiks! "' + email + '" does not seem like an email address')
-                return redirect('/register')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
 
         if user is not None:
-            error_register = 'User with ' + email + ' already exists'
-            return render_template('register.html', error_register=error_register)
+            error_register = 'User with ' + username + ' already exists'
+            return render_template('register.html', error_register=error_register, username=username)
 
         if request.form['password'] != request.form['verify']:
             error_register = "Passwords dont match"
-            return render_template('register.html', error_register=error_register)
+            return render_template('register.html', error_register=error_register, username=username)
 
-        if len(email) < 3 or len(email) > 20 or len(password) < 3 or len(password) > 20:
-            error_register = "an invalid username or an invalid password message"
-            return render_template('register.html', error_register=error_register)
+        if len(username) < 3 or len(username) > 20 or len(password) < 3 or len(password) > 20:
+            error_register = "invalid username or invalid password"
+            return render_template('register.html', error_register=error_register, username=username)
 
-        user = User(email=email, password=password)
+        user = User(username=username, password=hashlib.md5(password.encode()).hexdigest())
         db.session.add(user)
         db.session.commit()
-        session['email'] = email
+        session['username'] = username
         return redirect("/")
     else:
         return render_template('register.html')
 
 
-def is_email(string):
-    atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    if not atsign_present:
-        return False
-    else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
+# def is_email(string):
+#     atsign_index = string.find('@')
+#     atsign_present = atsign_index >= 0
+#     if not atsign_present:
+#         return False
+#     else:
+#         domain_dot_index = string.find('.', atsign_index)
+#         domain_dot_present = domain_dot_index >= 0
+#         return domain_dot_present
 
 
 @app.route("/logout")
 def logout():
-    del session['email']
+    del session['username']
     return redirect("/")
 
 
 @app.before_request
 def require_login():
 
-    endpoints_without_login = ['/', 'login', 'register']
+    endpoints_without_login = ['main_page', 'login', 'register']
 
-    if not (session or request.endpoint in endpoints_without_login):
+    if request.endpoint not in endpoints_without_login and 'username' not in session:
         return redirect("/register")
 
+    # if request.endpoint == 'addpost' and session not in User.query.filter_by(username=session['username']).all():
+    #     return redirect("/register")
+
+
+def sort():
+    print("hello")
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
